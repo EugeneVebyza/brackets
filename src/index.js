e70b7322176e8d4aa7b102a96e07d18fb67028b0
EugeneVebyza <36309477+EugeneVebyza@users.noreply.github.com>
2020-03-22 18:59:46 +0400

    init: add solve

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  if (str.length % 2 != 0) {
+    return false;
+  }
+  let result = str,s=0;
+    while (s < str.length){
+      s++
+      for (let b = 0; b < bracketsConfig.length; b++){
+        result = result.replace(bracketsConfig[b].join(''), '');
+      }
+  }
+  return result.length == 0;
+};
